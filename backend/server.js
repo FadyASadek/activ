c499a339d1c59e8ddb905fd9910489bd8d4f014b
Fady <alsyadf29@gmail.com>
2026-04-11 04:40:54 +0200

    fix json

--- backend/server.js
+++ backend/server.js
@@ -34,8 +34,8 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
-app.use(express.json());
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // 6️⃣ Multer setup for file uploads
 const upload = multer({ storage: multer.memoryStorage() });
 
